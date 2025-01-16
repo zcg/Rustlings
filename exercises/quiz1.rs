618ceca2b151d37a059250796bbd3734854a7999
zcg <gamer@live.cn>
2025-01-16 16:53:13 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,9 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
+fn calculate_price_of_apples(number :i32) -> i32{
+        if number <=40{
+            number*2
+        }else{
+            number
+        }
+}
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
